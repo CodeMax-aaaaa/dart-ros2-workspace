c02b3cc17d68055c06bc5687e9fc369670072f02
ChengHao <chengnangong@163.com>
2025-04-30 01:40:49 +0800

    子模组化dart_common_share

--- src/dart_launcher/include/node_dart_param_gateway.hpp
+++ src/dart_launcher/include/node_dart_param_gateway.hpp
@@ -8,8 +8,17 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_lifecycle/lifecycle_node.hpp>
 
+// Dart_msg
+#include <dart_msgs/msg/dart_param.hpp>
+#include <dart_msgs/msg/dart_launcher_status.hpp>
+
 // ROS2 Service
 #include <std_srvs/srv/empty.hpp>
+
+// Json
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 class NodeDartParamGateway : public rclcpp_lifecycle::LifecycleNode
 {
@@ -22,6 +31,16 @@ public:
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &previous_state) override;
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &previous_state) override;
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_error(const rclcpp_lifecycle::State &previous_state) override;
+
+private:
+    // ROS2 Lifecycle Node
+    rclcpp::Node::SharedPtr node_;
+
+    // Publisher
+    rclcpp::Publisher<dart_msgs::msg::DartStatus>::SharedPtr dart_param_pub_;
+
+    // Dart_param
+    dart_msgs::msg::DartParam dart_param_;
 };
 
 #endif
